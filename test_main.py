dfc13e3734a86052cb292411ce19538e73a2cff9
Roman <bekkerrdm@gmail.com>
2023-01-20 06:22:37 +0700

    fix

--- test_main.py
+++ test_main.py
@@ -1,9 +1,14 @@
+import os
 import sys
 
+import requests
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
 from some import privet
+
+VERSION = 1
+
 
 # Подкласс QMainWindow для настройки главного окна приложения
 class MainWindow(QMainWindow):
@@ -19,12 +24,40 @@ class MainWindow(QMainWindow):
         # Устанавливаем центральный виджет Window.
         self.setCentralWidget(button)
 
+        button.clicked.connect(self.upd)
+
+    def upd(self):
+        try:
+            link = "https://raw.githubusercontent.com/SomeUser/SomeRepo/main/SomeFolder/version.txt"
+            check = requests.get(link)
+
+            if float(VERSION) < float(check.text):
+                print('Обновление')
+                filename = os.path.basename(sys.argv[0])
+
+                for file in os.listdir():
+                    if file == filename:
+                        pass
+
+                    else:
+                        os.remove(file)
+
+                exename = f'NameOfYourApp{float(check.text)}.exe'
+                code = requests.get(
+                    "https://raw.githubusercontent.com/SomeUser/SomeRepo/main/SomeFolder/NewUpdate.exe",
+                    allow_redirects=True)
+                open(exename, 'wb').write(code.content)
+
+                os.remove(sys.argv[0])
+                sys.exit()
+
+            else:
+                print('Обвновлений нет')
+
+        except Exception as e:
+            print(e)
 
 app = QApplication(sys.argv)
-
-
-def upd():
-    pass
 
 
 def main():
@@ -34,12 +67,10 @@ def main():
     app.exec()
 
 
-update = False
 if __name__ == '__main__':
-    if not update:
-        print(privet)
-        main()
-    upd()
+    print(privet)
+    main()
+
 
 
 
